3dc2633a1697044ad8e6bc8f101d0430f38c85f6
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-02 13:16:18 +0900

    Fix for the latest

--- denops/ddc/filters/matcher_head.ts
+++ denops/ddc/filters/matcher_head.ts
@@ -5,13 +5,14 @@ import {
   Denops,
   DdcOptions,
   FilterOptions,
-} from "https://deno.land/x/ddc_vim@v0.0.5/types.ts";
+} from "https://deno.land/x/ddc_vim@v0.0.7/types.ts";
 
 export class Filter extends BaseFilter {
   filter(
     _denops: Denops,
     _context: Context,
     _options: DdcOptions,
+    _sourceOptions: SourceOptions,
     _filterOptions: FilterOptions,
     _filterParams: Record<string, unknown>,
     completeStr: string,
