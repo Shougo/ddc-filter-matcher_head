cfd3070548dbf0c43ca826bb1cc54721e73e3131
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-09-20 00:46:39 +0900

    Fix for the latest ddc

--- denops/@ddc-filters/matcher_head.ts
+++ denops/@ddc-filters/matcher_head.ts
@@ -2,7 +2,7 @@ import {
   BaseFilter,
   Candidate,
   SourceOptions,
-} from "https://deno.land/x/ddc_vim@v0.9.0/types.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.13.0/types.ts#^";
 
 export class Filter extends BaseFilter<{}> {
   filter(args: {
