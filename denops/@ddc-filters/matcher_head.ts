5790c1f011ac809cb5ad6af3fcf1714e3a7621ca
aileot <46470475+aileot@users.noreply.github.com>
2022-08-25 21:21:53 +0900

    feat(params): add option "maxMatchLength"

--- denops/@ddc-filters/matcher_head.ts
+++ denops/@ddc-filters/matcher_head.ts
@@ -4,25 +4,36 @@ import {
   SourceOptions,
 } from "https://deno.land/x/ddc_vim@v2.3.1/types.ts";
 
-type Params = Record<never, never>;
+type Params = {
+  maxMatchLength: number;
+};
 
 export class Filter extends BaseFilter<Params> {
   filter(args: {
     sourceOptions: SourceOptions,
+    filterParams: Record<string, unknown>,
     completeStr: string,
     items: Item[],
   }): Promise<Item[]> {
+    const maxMatchLength = args.filterParams.maxMatchLength as number
+    let str: string = maxMatchLength == 0
+      ? args.completeStr
+      : args.completeStr.slice(0, maxMatchLength)
     if (args.sourceOptions.ignoreCase) {
-      args.completeStr = args.completeStr.toLowerCase();
+      str = str.toLowerCase();
       return Promise.resolve(args.items.filter(
-        (item) => item.word.toLowerCase().startsWith(args.completeStr),
+        (item) => item.word.toLowerCase().startsWith(str),
       ));
     } else {
       return Promise.resolve(args.items.filter(
-        (item) => item.word.startsWith(args.completeStr),
+        (item) => item.word.startsWith(str),
       ));
     }
   }
 
-  params(): Params { return {}; }
+  params(): Params {
+    return {
+      maxMatchLength: 0,
+    };
+  }
 }
