28f60903e1a9f1010ec01575cb99c64e76683466
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-08-02 09:22:51 +0900

    Update deps

--- denops/@ddc-filters/matcher_head.ts
+++ denops/@ddc-filters/matcher_head.ts
@@ -1,8 +1,8 @@
 import {
   BaseFilter,
-  Item,
-  SourceOptions,
-} from "https://deno.land/x/ddc_vim@v4.3.1/types.ts";
+  type Item,
+  type SourceOptions,
+} from "jsr:@shougo/ddc-vim@6.0.0/types";
 
 type Params = {
   maxMatchLength: number;
